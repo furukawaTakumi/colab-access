9ff3a3d4ece194b2e5a64b22a2e6094c85283ca0
furukawaTakumi <black3moon88@gmail.com>
2020-05-15 23:04:05 +0900

    定期的にGoogle Colabにアクセスするスクリプトの追加

---
+++ access_colab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo "機械学習に使用しているgoogle colab urlを引数として与えてください。"
+  exit -1
+fi
+
+for count in `seq 12`
+do
+  echo "$count" `date '+%y/%m/%d %H:%M:%S'` "connected."
+  curl $1
+  sleep 3600
+done
